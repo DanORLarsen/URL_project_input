0b11cc48c34cb7dda40eb73c80c12b5c44b8dd37
DanOliverRunoLarsen <42993978+DanOliverRunoLarsen@users.noreply.github.com>
2018-11-07 12:13:00 +0100

    Skriv url, hvis fejl køre shittet videre pga catch

---
+++ src/sample/Controller.java
@@ -0,0 +1,51 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.awt.desktop.OpenURIHandler;
+import java.net.URL;
+
+import java.awt.*;
+import java.awt.desktop.OpenFilesEvent;
+import java.io.File;
+import java.io.IOException;
+
+public class Controller {
+    @FXML
+    TextField displayer;
+
+    @FXML
+    TextArea outputDisplay;
+
+
+    public void sendKommando(ActionEvent actionEvent) {
+        try {
+
+            String s = displayer.getText();
+            outputDisplay.setText((outputDisplay.getText() + "Du har skrevet: " + s + "\n"));
+            java.awt.Desktop.getDesktop().browse(java.net.URI.create(s));
+        }catch (Exception e){
+            System.out.println("Kan ikke finde siden");
+        }
+    }
+
+    public void clear(ActionEvent actionEvent) {
+        outputDisplay.clear();
+    }
+
+    public void getInfo(ActionEvent actionEvent) {
+
+        outputDisplay.setText(outputDisplay.getText() + "Navn: Dan" + "\nAlder: 21 år" + "\nUddannelse: DAT\n");
+    }
+
+    public void openTank(MouseEvent mouseEvent) throws IOException {
+        String htmlPath = "www.tanktrouble.com";
+        java.awt.Desktop.getDesktop().browse(java.net.URI.create(htmlPath));
+
+    }
+}
